7897a0e373fd14c391ed4d9ef4b581b9c6c70035
Ben V. Brown <Ralim@Ralimtek.com>
2021-07-25 19:13:06 +1000

    WiP doing full reset

--- lora/start.sh
+++ lora/start.sh
@@ -1,19 +1,45 @@
 #! /bin/bash
+SX1301_W_DISABLE_PIN=229
+SX1301_PWRKEY_PIN=72
+SX1301_RESET_PIN=71
+
+# Export all of the pins
+[[ -d "/sys/class/gpio/gpio$SX1301_W_DISABLE_PIN/" ]] || echo "$SX1301_W_DISABLE_PIN"  > /sys/class/gpio/export
+[[ -d "/sys/class/gpio/gpio$SX1301_PWRKEY_PIN/" ]] || echo "$SX1301_PWRKEY_PIN"  > /sys/class/gpio/export
+[[ -d "/sys/class/gpio/gpio$SX1301_RESET_PIN/" ]] || echo "$SX1301_RESET_PIN"  > /sys/class/gpio/export
+# Set all pins to outputs
+echo "out" > /sys/class/gpio/gpio$SX1301_W_DISABLE_PIN/direction
+echo "out" > /sys/class/gpio/gpio$SX1301_PWRKEY_PIN/direction
+echo "out" > /sys/class/gpio/gpio$SX1301_RESET_PIN/direction
+echo "0"   > /sys/class/gpio/gpio$SX1301_W_DISABLE_PIN/value
+echo "0"   > /sys/class/gpio/gpio$SX1301_PWRKEY_PIN/value
+echo "1"   > /sys/class/gpio/gpio$SX1301_RESET_PIN/value
+
+# Pulse wireless disable signal
+echo "0"   > /sys/class/gpio/gpio$SX1301_W_DISABLE_PIN/value
+sleep 0.5
+echo "1"   > /sys/class/gpio/gpio$SX1301_W_DISABLE_PIN/value
+sleep 0.2
+echo "0"   > /sys/class/gpio/gpio$SX1301_W_DISABLE_PIN/value
+sleep 0.5
+# Pulse pwr
+echo "0"   > /sys/class/gpio/gpio$SX1301_PWRKEY_PIN/value
+sleep 0.5
+echo "1"   > /sys/class/gpio/gpio$SX1301_PWRKEY_PIN/value
+sleep 0.2
+echo "0"   > /sys/class/gpio/gpio$SX1301_PWRKEY_PIN/value
+sleep 0.5
 
 
-# Reset iC880a PIN
-SX1301_RESET_BCM_PIN=71		# edited for Pine64 (from RPI 17)
-echo "$SX1301_RESET_BCM_PIN"  > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction
-echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
+# Pulse reset
+echo "1"   > /sys/class/gpio/gpio$SX1301_RESET_PIN/value
 sleep 0.1
-echo "1"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
+echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_PIN/value
 sleep 0.1
-echo "0"   > /sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
-sleep 0.1
-echo "$SX1301_RESET_BCM_PIN" > /sys/class/gpio/unexport
+echo "1"   > /sys/class/gpio/gpio$SX1301_RESET_PIN/value
+sleep 2
 ./set_eui.sh
 sleep 0.2
 #./update_gwid.sh ./local_conf.json
 sleep 0.5
-./lora_pkt_fwd
+#./lora_pkt_fwd
